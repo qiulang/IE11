93e18b398c03c1230d29b2c615ff70eb51541539
qiulang@macbook3 <qiulang2000@126.com>
2019-11-15 09:57:50 +0800

    "transpileDependencies": ["debug"]  造成 "export 'default' (imported as 'debug') was not found in 'debug'

--- babel.config.js
+++ babel.config.js
@@ -15,4 +15,5 @@ module.exports = {
       }
     ]
   ]
+  // ignore: [/node_modules\/(?!debug)/]
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 // import "core-js";
+import "@babel/polyfill";
+
+localStorage.debug = "app:*";
 
 import debug from "debug";
+// let debug = require("debug");
 const info = debug("app:info");
 
 info("hi debug");
