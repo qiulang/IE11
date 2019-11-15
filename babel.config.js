83313413d5750698d4a6908c73048922f4264de5
qiulang@macbook3 <qiulang2000@126.com>
2019-11-15 12:28:38 +0800

    overrides: 也不管用

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,39 @@
 module.exports = {
-  presets: [
-    "@vue/cli-plugin-babel/preset",
-    [
-      "@babel/preset-env",
-      {
-        useBuiltIns: "entry",
-        debug: true,
-        corejs: 3,
-        modules: false,
-        targets: {
-          chrome: "68",
-          ie: "11"
-        }
-      }
-    ]
+  overrides: [
+    {
+      test: "./node_modules/debug/**/*.js",
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            modules: "commonjs",
+            useBuiltIns: "entry",
+            debug: true,
+            corejs: 3,
+            targets: {
+              chrome: "68",
+              ie: "11"
+            }
+          }
+        ]
+      ]
+    }
   ]
+  // presets: [
+  //   "@vue/cli-plugin-babel/preset",
+  //   [
+  //     "@babel/preset-env",
+  //     {
+  //       useBuiltIns: "entry",
+  //       debug: true,
+  //       corejs: 3,
+  //       modules: false,
+  //       targets: {
+  //         chrome: "68",
+  //         ie: "11"
+  //       }
+  //     }
+  //   ]
+  // ]
   // ignore: [/node_modules\/(?!debug)/]
 };
