7ac9028176a85e79828b8db12c6f2f49a6a3265d
qiulang@macbook3 <qiulang2000@126.com>
2019-11-17 11:38:22 +0800

    终于IE可以显示了！overrides + presets

--- babel.config.js
+++ babel.config.js
@@ -7,9 +7,6 @@ module.exports = {
           "@babel/preset-env",
           {
             modules: "commonjs",
-            useBuiltIns: "entry",
-            debug: true,
-            corejs: 3,
             targets: {
               chrome: "68",
               ie: "11"
@@ -18,22 +15,21 @@ module.exports = {
         ]
       ]
     }
+  ],
+  presets: [
+    "@vue/cli-plugin-babel/preset",
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        debug: true,
+        corejs: 3,
+        modules: false,
+        targets: {
+          chrome: "68",
+          ie: "11"
+        }
+      }
+    ]
   ]
-  // presets: [
-  //   "@vue/cli-plugin-babel/preset",
-  //   [
-  //     "@babel/preset-env",
-  //     {
-  //       useBuiltIns: "entry",
-  //       debug: true,
-  //       corejs: 3,
-  //       modules: false,
-  //       targets: {
-  //         chrome: "68",
-  //         ie: "11"
-  //       }
-  //     }
-  //   ]
-  // ]
-  // ignore: [/node_modules\/(?!debug)/]
 };
